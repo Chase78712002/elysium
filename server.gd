ffdbbfbada812af7f1a12d6438c3cbbba0ce37c7
Alvin Lin <alvinch.lin@gmail.com>
2026-02-27 16:07:14 -0800

    Add: server headless boot + client connects (UDP 32100)

--- server.gd
+++ server.gd
@@ -8,6 +8,8 @@ const MAX_CLIENTS := 32
 func _ready() -> void:
 	if DisplayServer.get_name() == "headless":
 		start_server()
+	else:
+		connect_to_server("127.0.0.1")
 	
 func start_server() -> void:
 	var peer := ENetMultiplayerPeer.new()
@@ -19,8 +21,24 @@ func start_server() -> void:
 
 	multiplayer.multiplayer_peer = peer
 
-	multiplayer.connected_to_server.connect(func(): print("✅ connected to server"))
+	multiplayer.peer_connected.connect(func(id): print("✅ peer connected:", id))
+	multiplayer.peer_disconnected.connect(func(id): print("👋 peer disconnected:",id))
 	print("✅ Server Listening (UPD) on port %d" % PORT)
+
+func connect_to_server(ip: String) -> void:
+	var peer := ENetMultiplayerPeer.new()
+	var error := peer.create_client(ip, PORT)
+
+	if error != OK:
+		push_error("Failed to create client %s" % error)
+		return
+
+	multiplayer.multiplayer_peer = peer
+	print("✅ Client connecting to %s:%d" % [ip, PORT])
+
+	multiplayer.connected_to_server.connect(func(): print("✅ connected to server"))
+	multiplayer.connection_failed.connect(func(): print("💔 connection failed:"))
+	multiplayer.server_disconnected.connect(func(): print("⚠️ server disconnected:"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
