e972e1aad76de94b3cd6ef75e9aad508fb85763f
Alvin Lin <alvinch.lin@gmail.com>
2026-04-24 16:12:40 -0700

    Add:Creature spawn positions data into game data file

--- game_data.gd
+++ game_data.gd
@@ -7,3 +7,11 @@ const SPAWN_POINTS := [
 	Vector2(200, 400),
 	Vector2(400, 400),
 ]
+
+const CREATURE_SPAWN_POSITIONS: Array[Vector2] = [
+	Vector2(281, -115),
+	Vector2(-400, 200),
+	Vector2(500, 300),
+	Vector2(-200, -300),
+	Vector2(600, -200),
+]
