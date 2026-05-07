74205090b9db6cd1ef1dae7579559e14ff48dc85
Alvin Lin <alvinch.lin@gmail.com>
2026-05-07 11:29:50 -0700

    Add: Creature respawning after death

--- creature.gd
+++ creature.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 const MAX_HP = 10
 var hp = MAX_HP
+var spawn_position: Vector2 = Vector2.ZERO
 
 @rpc("any_peer", "call_local","reliable")
 func take_damage(amount:int) -> void:
@@ -10,6 +11,21 @@ func take_damage(amount:int) -> void:
 	prints(name, "HP:", hp)
 	
 	if hp <= 0:
-		if multiplayer.is_server():
-			queue_free()
 		prints(name, "is dead")
+		if multiplayer.is_server():
+			respawn()
+
+func _ready() -> void:
+	spawn_position = global_position
+	
+@rpc("authority","call_local","reliable")
+func set_visiblity(value: bool) -> void:
+	visible = value
+	
+func respawn() -> void:
+	set_visiblity.rpc(false)
+	prints("Respawning...")
+	await get_tree().create_timer(5.0).timeout
+	hp = MAX_HP
+	global_position = spawn_position
+	set_visiblity.rpc(true)
