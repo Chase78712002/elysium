ba51df792aa026b7bfa26a0ee806e8102aaa537d
Alvin Lin <alvinch.lin@gmail.com>
2026-05-11 15:58:40 -0700

    Add: creatures attack loop to attack back at player when in range

--- creature.gd
+++ creature.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 const MAX_HP = 10
+const ATTACK_RANGE: float = 150.0
+const ATTACK_DAMAGE: int = 5
 var hp = MAX_HP
 var spawn_position: Vector2 = Vector2.ZERO
 
@@ -17,7 +19,9 @@ func take_damage(amount:int) -> void:
 
 func _ready() -> void:
 	spawn_position = global_position
-	
+	if multiplayer.is_server():
+		attack_loop()	
+
 @rpc("authority","call_local","reliable")
 func set_visiblity(value: bool) -> void:
 	visible = value
@@ -29,3 +33,13 @@ func respawn() -> void:
 	hp = MAX_HP
 	global_position = spawn_position
 	set_visiblity.rpc(true)
+
+func attack_loop() -> void:
+	while true:
+		await get_tree().create_timer(2.0).timeout	
+		if not visible:
+			continue
+		for player in get_tree().get_nodes_in_group("players"):
+			var dist = global_position.distance_to(player.global_position)	
+			if dist <= ATTACK_RANGE:
+				player.take_damage.rpc(ATTACK_DAMAGE)
